14b5c14221d527bb73f77475671c5dce8406bddb
Joseph Schartung <joseph.schartung@gmail.com>
2022-06-26 02:21:55 -0700

    navbar redirects to homepage now

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: NextPage = () => {
     return (
         <header className="text-gray-400 bg-gray-900 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <Link href="" >
+                <Link href="/">
                     <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
                         <MdEnhancedEncryption size={50} />
                         <span className="ml-3 text-xl">
